e6fd14e484e7f3c0bd845ab010e5ce117456159c
jassyio <joejassyio@gmail.com>
2024-03-06 17:14:36 +0300

    5 photo

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,3 @@
 export default function uploadPhoto(filename) {
     return Promise.reject(Error(`${filename} cannot be processed`));
   }
-export default function uploadPhoto(filename) {
-  return Promise.reject(Error(`${filename} cannot be processed`));
-}
