9fa04dffff863477d648a96dbcbeb75b628c56e5
jassyio <joejassyio@gmail.com>
2024-03-06 17:32:16 +0300

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,29 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const values_1 = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
-  const arr = [];
-  for (const item of values_1) {
-    arr.push({ status: item.status, value: item.value || item.reason });
+  const user = {
+    status: 'pending',
+    value: '',
+  };
+  const photo = {
+    status: 'pending',
+    value: '',
+  };
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    user.status = 'fulfilled';
+    user.value = signup;
+  } catch (error) {
+    user.status = 'rejected';
+    user.value = error.toString();
   }
-  return arr;
+  try {
+    const upload = await uploadPhoto(fileName);
+    photo.status = 'fulfilled';
+    photo.value = upload;
+  } catch (error) {
+    photo.status = 'rejected';
+    photo.value = error.toString();
+  }
+  return [user, photo];
 }
